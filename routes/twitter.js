8ebd55cf30d416cdaf7ecaf0079278fdb118614b
Nicolas Vasquez <51717304+vasquezn15@users.noreply.github.com>
2021-03-10 16:42:26 -0500

    Initial commit for front end update functinality

--- routes/twitter.js
+++ routes/twitter.js
@@ -11,6 +11,7 @@ const {
     oauthGetUserById,
   } = require("../oauth/oauth-utils");
 
+  
 function twitter(method = "authorize") {
     return async (req, res) => {
       console.log(`/twitter/${method}`);
@@ -26,7 +27,7 @@ function twitter(method = "authorize") {
         results,
       });
 
-      req.session = req.session || {};
+      // req.session = req.session || {};
       req.session.oauthRequestToken = oauthRequestToken;
       req.session.oauthRequestTokenSecret = oauthRequestTokenSecret;
 
@@ -34,17 +35,29 @@ function twitter(method = "authorize") {
       console.log("redirecting user to ", authorizationUrl);
       res.redirect(authorizationUrl);
     };
-  }
+}
+  
+router.get('/route1', (req, res) => {
+  req.session.route1 = 'test route'
+  console.log(req.session);
+  res.send('This is the response')
+})
+
+router.get('/route2', (req,res) => {
+  console.log(req.session);
+  res.send('This is the response2')  })
 
 router.get("/twitter/logout", logout);
+
   function logout(req, res, next) {
-    res.clearCookie("twitter_screen_name");
-    req.session.destroy(() => res.redirect("http://localhost:3000/"));
+    // res.clearCookie("twitter_screen_name");
+    // req.session.destroy(() => res.redirect("http://localhost:3000/"));
   }
 
-  router.get("/twitter/authoriz", twitter());
+
+  router.get("/twitter/authorize", twitter());
   
-  router.get("/auth/oauth/callback", async (req, res) => {
+  router.get("/auth/oauth/callback", async (req, res, next) => {
     const { oauthRequestToken, oauthRequestTokenSecret } = req.session;
     const { oauth_verifier: oauthVerifier } = req.query;
     console.log("/twitter/callback", {
@@ -66,6 +79,7 @@ router.get("/twitter/logout", logout);
 
     const { user_id: userId } = results;
     req.session.userId = userId;
+
     const user = await oauthGetUserById(userId, {
       oauthAccessToken,
       oauthAccessTokenSecret,
@@ -77,33 +91,38 @@ router.get("/twitter/logout", logout);
       httpOnly: true,
     });
 
+    res.cookie("twitter_accesstoken", oauthAccessToken, {
+      maxAge: 900000,
+      httpOnly: true,
+    });
+
+    console.log("Access Token: ", oauthAccessToken)
+    console.log("Access Token Secret: ", oauthAccessTokenSecret)
     console.log("user succesfully logged in with twitter", user.screen_name);
-    req.session.save(() =>
-      res.redirect(
-        "http://localhost:3000/?user_id=" +
-          userId +
-          "&username=" +
-          user.screen_name
-      )
-    );
+    
+      res.redirect("//localhost:3000/loggedin")
+  //   }
+  // );
+    next();
   });
 
-  router.get("/twitter/followers", (req, res) => {
-    const userId = req.query.user_id || "1050522049947545600";
-    axios
+router.get("/twitter/followers", (req, res) => {
+    const userId = req.query.user_id;
+    console.log("UserID: " ,userId)
+  axios
       .get(`https://api.twitter.com/2/users/${userId}/followers`, {
         headers: {
           Authorization: "Bearer " + keys.TWITTER_BEARER_TOKEN, //the token is a variable which holds the token
         },
       })
       .then((response) => {
-        res.send(response.data);
+        res.send(JSON.stringify(response.data));
+
         jsonexport(
           response.data.data,
           { rowDelimiter: ";" },
           function (err, csv) {
             if (err) return console.error(err);
-            console.log(csv);
             fs.writeFile("./data/test.csv", csv, (err) => {
               if (err) {
                 console.error(err);
@@ -112,15 +131,15 @@ router.get("/twitter/logout", logout);
             });
           }
         );
+
       })
       .catch((error) => {
         console.log(error);
       });
   });
 
-  router.get("/twitter/following", (req, res) => {
+router.get("/twitter/following", (req, res) => {
     const userId = req.query.user_id;
-    console.log("UserID = ", userId);
     axios
       .get(`https://api.twitter.com/2/users/${userId}/following`, {
         headers: {
@@ -134,5 +153,35 @@ router.get("/twitter/logout", logout);
         console.log(error);
       });
   });
+
+  function getcookie(req) {
+    var cookie = req.headers.cookie;
+    // user=someone; session=QyhYzXhkTZawIb5qSl3KKyPVN (this is my cookie i get)
+    return cookie.split('; ');
+}
+
+router.get('/twitter/unfollow', (req, res) => {
+  
+
+  const oauthAccessToken = req.session.oauthAccessToken;
+  const userId = req.query.user_id
+  const target_user_id = req.query.follower_id;
+  console.log("Target_UserID = ", target_user_id);
+  console.log("UserID = ", userId);
+  var cookie = getcookie(req);
+  console.log("Access Token = ", cookie);
+  
+//   axios.delete(`https://api.twitter.com/2/users/${userId}/following/${target_user_id}`, {
+//     headers: {
+//       'Authorization': `OAuth oauth_consumer_key=${keys.TWITTER_CONSUMER_KEY},oauth_token=${req.session.oauthAccessToken},oauth_signature_method="HMAC-SHA1",oauth_timestamp="1615061519",oauth_nonce="D38X0s5XkMN",oauth_version="1.0",oauth_signature="nvFbuafY8m9vjB16x9PGRDm1vMI%3D"`    }
+//   })
+//     .then((response) => {
+//       console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+})
 
 module.exports = router;
